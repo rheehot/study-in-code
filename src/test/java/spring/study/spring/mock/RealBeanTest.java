674f7a026b55073f0b5e49734d7b8353dcd0c451
Jsing <joosing711@gmail.com>
2022-09-21 12:56:07 +0900

    Spring > Mock 학습

---
+++ src/test/java/spring/study/spring/mock/RealBeanTest.java
@@ -0,0 +1,19 @@
+package spring.study.spring.mock;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {SampleBean.class})
+public class RealBeanTest {
+    @Autowired
+    SampleBean realBean;
+
+    @Test
+    @DisplayName("Real Bean 테스트")
+    void realBeanHello() {
+        Assertions.assertEquals("hello", realBean.hello());
+    }
+}
---
+++ src/test/java/spring/study/spring/mock/SampleBean.java
@@ -0,0 +1,10 @@
+package spring.study.spring.mock;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleBean {
+    public String hello() {
+        return "hello";
+    }
+}
