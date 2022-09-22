1f0573d20ca808953f183484dbe6232901ccecb2
Jsing <joosing711@gmail.com>
2022-09-22 15:57:24 +0900

    SpringUtils#remove 학습 테스트

---
+++ src/test/java/spring/study/string/StringUtilsTest.java
@@ -0,0 +1,36 @@
+package spring.study.string;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("StringUtils")
+public class StringUtilsTest {
+    @Test
+    void hello() {
+        System.out.println("hello");
+    }
+
+    @Nested
+    class Remove {
+        @Test
+        @DisplayName("""
+                remove("ABCD", "CD") = "AB"
+                """)
+        void givenABCD_whenRemoveCD_thenEqualAB() {
+            String sample = "ABCD";
+            Assertions.assertEquals("AB", StringUtils.remove(sample, "CD"));
+        }
+
+        @Test
+        @DisplayName("""
+                remove("ABCD", "AD") = "ABCD"
+                """)
+        void givenABCD_whenRemoveAD_thenEqualABCD() {
+            String sample = "ABCD";
+            Assertions.assertEquals("ABCD", StringUtils.remove(sample, "AD"));
+        }
+    }
+}
