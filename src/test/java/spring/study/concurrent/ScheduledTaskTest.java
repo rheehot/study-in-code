ab390c8e15d11c1edbd8f7336d376eeec9c37cb7
Jsing <joosing711@gmail.com>
2022-09-29 13:31:47 +0900

    concurrent > ScheduledExecutorService > cancel 학습

---
+++ src/test/java/spring/study/concurrent/ScheduledTaskTest.java
@@ -0,0 +1,27 @@
+package spring.study.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class ScheduledTaskTest {
+    @Test
+    void cancelTest() {
+        // Given
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+        // When : 타임 스탬프 태스크 시작 - 대기 - 취소 - 대기
+        ScheduledFuture<?> future = executor.scheduleAtFixedRate(() ->
+                log.info("hello"), 0, 100, TimeUnit.MILLISECONDS);
+
+        future.cancel(true);
+        Assertions.assertTrue(future.isCancelled());
+        Assertions.assertTrue(future.isDone());
+    }
+}
