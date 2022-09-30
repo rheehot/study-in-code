a4a86a3de460a15e7de39de0bd701d0b74da3e9d
Jsing <joosing711@gmail.com>
2022-09-30 10:57:34 +0900

    Time > LocalDateTime > plusNano() 메서드의 초 값 영향성 학습

--- src/test/java/spring/study/time/LocalDateTimeTest.java
+++ src/test/java/spring/study/time/LocalDateTimeTest.java
@@ -2,12 +2,16 @@ package spring.study.time;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.Period;
-import java.util.concurrent.*;
+import java.time.Month;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class LocalDateTimeTest {
@@ -34,9 +38,7 @@ public class LocalDateTimeTest {
         CopyOnWriteArrayList<LocalDateTime> timeStampList = new CopyOnWriteArrayList<>();
 
         // When : N msec 간격으로 타임스탬프
-        executor.scheduleAtFixedRate(() -> {
-            timeStampList.add(LocalDateTime.now());
-        }, 0, intervalMsec, TimeUnit.MILLISECONDS);
+        executor.scheduleAtFixedRate(() -> timeStampList.add(LocalDateTime.now()), 0, intervalMsec, TimeUnit.MILLISECONDS);
 
         // Then : N msec 간격 확인 (M msec 마진 허용)
         Thread.sleep(intervalMsec * 10);
@@ -47,4 +49,16 @@ public class LocalDateTimeTest {
         }
     }
 
+    @Test
+    @DisplayName("1.900(sec) plusNano 0.1(sec) = 2.000(sec)")
+    void plusNanaTest() {
+        int testSecond = 56;
+        LocalDateTime sample = LocalDateTime.of(2022, Month.AUGUST, 30, 12, 34, testSecond, toNano(900));
+        Assertions.assertEquals(testSecond + 1, sample.plusNanos(toNano(100)).getSecond());
+        Assertions.assertEquals(toNano(0), sample.plusNanos(toNano(100)).getNano());
+    }
+
+    private int toNano(int millis) {
+        return millis * 1000 * 1000;
+    }
 }
