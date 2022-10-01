fbc09d6a3864993327c6a9ab4571077402980a9c
Jsing <joosing711@gmail.com>
2022-10-01 19:25:47 +0900

    Thread > 학습 레퍼런스 코멘트

--- src/test/java/spring/study/thread/ThreadPoolTest.java
+++ src/test/java/spring/study/thread/ThreadPoolTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * <a href="https://www.baeldung.com/thread-pool-java-and-guava">Introduction to Thread Pools in Java</a> 사이트를 학습합니다.
+ */
 @Slf4j
 public class ThreadPoolTest {
     @Test
