6f66f6f96089e816bb8db86db9eb860a7ae31353
Jsing <joosing711@gmail.com>
2022-10-15 08:19:49 +0900

    feat : Spring > Context > @DirtiesContext 학습

---
+++ src/test/java/spring/study/spring/context/User.java
@@ -0,0 +1,6 @@
+package spring.study.spring.context;
+
+public class User {
+    String firstName;
+    String lastName;
+}
---
+++ src/test/java/spring/study/spring/context/UserCache.java
@@ -0,0 +1,19 @@
+package spring.study.spring.context;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.Getter;
+
+public class UserCache {
+    @Getter
+    private Set<String> userList = new HashSet<>();
+
+    public boolean addUser(String user) {
+        return userList.add(user);
+    }
+
+    public void printUserList(String message) {
+        System.out.println(message + ": " + userList);
+    }
+}
