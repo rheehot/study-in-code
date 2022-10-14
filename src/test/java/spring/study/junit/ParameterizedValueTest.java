36a5a80bd5ce9e0c00b76681783f293dc617dd94
Jsing <joosing711@gmail.com>
2022-10-14 22:45:10 +0900

    feat : JUnit > @ParameterizedTest > @ValueSource 학습

--- src/test/java/spring/study/junit/ParameterizedValueTest.java
+++ src/test/java/spring/study/junit/ParameterizedValueTest.java
@@ -1,18 +1,19 @@
 package spring.study.junit;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParameterizedValueTest {
     @ParameterizedTest
     @MethodSource("testCaseSupplier")
-    void test(float[] expected, float[] actual) {
+    void methodSource(float[] expected, float[] actual) {
         System.out.println(Arrays.toString(expected));
         System.out.println(Arrays.toString(actual));
     }
@@ -22,5 +23,11 @@ public class ParameterizedValueTest {
                 arguments(new float[]{1, 2, 3}, new float[]{4, 5, 6}),
                 arguments(new float[]{1, 2, 3}, new float[]{4, 5, 6})
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1.0, 2.0, 3.0})
+    void valueSource(double value) {
+        System.out.println(value);
     }
 }
