80b2314262c563096d8bd4c2208617843d5a537d
Jsing <joosing711@gmail.com>
2022-10-14 22:51:17 +0900

    feat : JUnit > @TestMethodOrder & @Order 학습

---
+++ src/test/java/spring/study/junit/MethodOrderTest.java
@@ -0,0 +1,28 @@
+package spring.study.junit;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class MethodOrderTest {
+
+    @Test
+    @Order(3)
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    @Order(1)
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+    @Order(2)
+    public void test3() {
+        System.out.println("test3");
+    }
+}
