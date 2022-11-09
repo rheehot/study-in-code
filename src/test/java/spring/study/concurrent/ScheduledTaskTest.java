44dd9a2fa4352990d6df47de0205bbefe862b281
Jsing <joosing711@gmail.com>
2022-11-09 11:30:50 +0900

    feat : ScheduledExecutorService 테스트 재확인

--- src/test/java/spring/study/concurrent/ScheduledTaskTest.java
+++ src/test/java/spring/study/concurrent/ScheduledTaskTest.java
@@ -12,16 +12,17 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class ScheduledTaskTest {
     @Test
-    void cancelTest() {
+    void cancelTest() throws InterruptedException {
         // Given
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
         // When : 타임 스탬프 태스크 시작 - 대기 - 취소 - 대기
         ScheduledFuture<?> future = executor.scheduleAtFixedRate(() ->
                 log.info("hello"), 0, 100, TimeUnit.MILLISECONDS);
-
+        Thread.sleep(5000);
         future.cancel(true);
         Assertions.assertTrue(future.isCancelled());
         Assertions.assertTrue(future.isDone());
+        Thread.sleep(3000);
     }
 }
