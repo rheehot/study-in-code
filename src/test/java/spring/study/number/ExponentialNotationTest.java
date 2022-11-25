f9f31e4b19f91eadd222c1ec42f387e5ef1eae16
Jsing <joosing711@gmail.com>
2022-11-25 19:18:03 +0900

    feat: 지수 표기법을 통한 long 형 값 할당

--- src/test/java/spring/study/number/ExponentialNotationTest.java
+++ src/test/java/spring/study/number/ExponentialNotationTest.java
@@ -9,10 +9,12 @@ public class ExponentialNotationTest {
     @Test
     void exponentialNotation() {
         double expNumber = 5e+8;
+        long expInt = (long) 5e+12;
         double number = 500000000.0;
         log.info("expNumber: {}", expNumber);
         log.info("number: {}", number);
         Assertions.assertEquals(500000000, number);
+        Assertions.assertEquals(5000000000000L, expInt);
         Assertions.assertEquals(500000000, expNumber);
     }
 }
