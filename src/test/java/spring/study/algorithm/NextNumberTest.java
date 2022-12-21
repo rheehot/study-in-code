6577996905584474f947568d0bb532e6e91ec6d1
Jsing <joosing711@gmail.com>
2022-12-22 06:36:24 +0900

    feat: 이름 개선

--- src/test/java/spring/study/algorithm/NextNumberTest.java
+++ src/test/java/spring/study/algorithm/NextNumberTest.java
@@ -3,7 +3,10 @@ package spring.study.algorithm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
+/*
+ 리팩토링
+ -
+ */
 public class NextNumberTest {
     @Test
     void tester() {
@@ -17,45 +20,46 @@ public class NextNumberTest {
     - 등차 또는 등비를 구합니다.
     - 마지막 항목에서 등차 또는 등비를 사용해 해답을 구합니다.
      */
-    enum Type {
-        ARITHMETICAL, GEOMETRIC
+    enum SequenceType {
+        ARITHMETICAL, // 등차수열
+        GEOMETRIC     // 등비수열
     }
 
-    public int solution(int[] common) {
-        Type type = checkType(common);
-        int commonParam = checkCommonParam(common, type);
-        return checkNextNum(common[common.length-1], type, commonParam);
+    public int solution(int[] sequence) {
+        SequenceType sequenceType = checkSequenceType(sequence);
+        int regularParam = calculateRegularParam(sequence, sequenceType);
+        return getNextNum(sequence[sequence.length - 1], sequenceType, regularParam);
     }
 
-    private Type checkType(int[] common) {
-        assert common.length > 2;
+    private SequenceType checkSequenceType(int[] sequence) {
+        assert sequence.length > 2;
 
-        int diff = common[1] - common[0];
-        if (common[2] - common[1] == diff) {
-            return Type.ARITHMETICAL;
+        int diff = sequence[1] - sequence[0];
+        if (sequence[2] - sequence[1] == diff) {
+            return SequenceType.ARITHMETICAL;
         } else {
-            return Type.GEOMETRIC;
+            return SequenceType.GEOMETRIC;
         }
     }
 
-    private int checkCommonParam(int[] common, Type type) {
-        assert type == Type.ARITHMETICAL || type == Type.GEOMETRIC;
-        assert common[0] != 0;
+    private int calculateRegularParam(int[] sequence, SequenceType type) {
+        assert type == SequenceType.ARITHMETICAL || type == SequenceType.GEOMETRIC;
+        assert sequence[0] != 0;
 
-        if (type == Type.ARITHMETICAL) {
-            return common[1] - common[0];
+        if (type == SequenceType.ARITHMETICAL) {
+            return sequence[1] - sequence[0];
         } else {
-            return common[1] / common[0];
+            return sequence[1] / sequence[0];
         }
     }
 
-    private int checkNextNum(int lastItem, Type type, int commonParam) {
-        assert type == Type.ARITHMETICAL || type == Type.GEOMETRIC;
+    private int getNextNum(int lastItem, SequenceType type, int regularParam) {
+        assert type == SequenceType.ARITHMETICAL || type == SequenceType.GEOMETRIC;
 
-        if (type == Type.ARITHMETICAL) {
-            return lastItem + commonParam;
+        if (type == SequenceType.ARITHMETICAL) {
+            return lastItem + regularParam;
         } else {
-            return lastItem * commonParam;
+            return lastItem * regularParam;
         }
     }
 }
