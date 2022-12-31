ce4bef85469b44fd97e10faccda4fdd00f066dfa
Jsing <joosing711@gmail.com>
2022-12-31 22:56:38 +0900

    refactor: 문제의 제약사항을 Assertion을 통해 명확히 하여 나의 이해를 돕기

--- src/test/java/spring/study/solving/SmallSubStringTest.java
+++ src/test/java/spring/study/solving/SmallSubStringTest.java
@@ -13,6 +13,10 @@ public class SmallSubStringTest {
     }
 
     public int solution(String target, String sub) {
+        assert sub.length() >= 1 && sub.length() <= 18;
+        assert target.length() >= sub.length() && target.length() <= 10000;
+        assert sub.charAt(0) != '0' && target.charAt(0) != '0';
+
         return (int) IntStream.range(0, target.length() - sub.length() + 1)
                               .mapToObj(startIndex -> target.substring(startIndex, startIndex + sub.length()))
                               .map(Long::parseLong)
