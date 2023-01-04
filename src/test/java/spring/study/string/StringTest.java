b0b506a4e18dc3c83c95795eaa8f1f25f4bc8718
Jsing <joosing711@gmail.com>
2023-01-04 16:00:04 +0900

    feat: "123,456,789".split(",") 결과 테스트

--- src/test/java/spring/study/string/StringTest.java
+++ src/test/java/spring/study/string/StringTest.java
@@ -19,4 +19,15 @@ public class StringTest {
             Assertions.assertEquals("BC", sample.replaceAll("[AD]", ""));
         }
     }
+
+    @Nested
+    @DisplayName("Split")
+    class Split {
+        @Test
+        @DisplayName("""
+                "123,456,789".split(",") returns "123", "456", "789"\040""")
+        void split(){
+            Assertions.assertArrayEquals(new String[]{"123", "456", "789"}, "123,456,789".split(","));
+        }
+    }
 }
