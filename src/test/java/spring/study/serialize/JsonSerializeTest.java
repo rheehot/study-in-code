4275af09ac12e6a14fb94d1ee90d3be8fd7adc6a
Jsing <joosing711@gmail.com>
2023-01-12 19:19:35 +0900

    feat: JSON object, array 역직렬화

--- src/test/java/spring/study/serialize/JsonSerializeTest.java
+++ src/test/java/spring/study/serialize/JsonSerializeTest.java
@@ -1,30 +1,113 @@
 package spring.study.serialize;
 
+import java.io.IOException;
+import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class JsonSerializeTest {
+
+    @Test
+    void objectDeserialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String carJson = """
+        {
+            "name" : "스파크",
+            "age" : 2014,
+            "velocity" : 120.12
+        }
+        """;
+
+        Car car = mapper.readValue(carJson, Car.class);
+        System.out.println("car = " + car);
+        Assertions.assertEquals("스파크", car.getName());
+        Assertions.assertEquals(2014, car.getAge());
+        Assertions.assertEquals(120.12, car.getVelocity());
+    }
+
+    @Test
+    void arrayDeserialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonCommands = """
+        [
+            "HI", "HELLO"
+        ]
+        """;
+
+        List<String> comments = mapper.readValue(jsonCommands, new TypeReference<>() {});
+        Assertions.assertEquals(2, comments.size());
+        Assertions.assertEquals("HI", comments.get(0));
+        Assertions.assertEquals("HELLO", comments.get(1));
+    }
+
+    @Test
+    void objectArrayDeserialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonCommands = """
+        [
+            {
+                "name" : "스파크",
+                "age" : 2014,
+                "velocity" : 120.12
+            },
+            {
+                "name" : "그랜저",
+                "age" : 2015,
+                "velocity" : 140.14
+            }
+        ]
+        """;
+
+        List<Car> comments = mapper.readValue(jsonCommands, new TypeReference<>() {});
+        Assertions.assertEquals(2, comments.size());
+        Assertions.assertEquals(new Car("스파크", 2014, 120.12), comments.get(0));
+        Assertions.assertEquals(new Car("그랜저", 2015, 140.14), comments.get(1));
+    }
+
+    @Test
+    void innerClassDeserializeException() throws JsonProcessingException {
+        @ToString
+        @Getter
+        class Car {
+            private String name;
+            private double velocity;
+            private int age;
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        String carJson = """
+                {
+                    "name" : "스파크",
+                    "age" : 2014,
+                    "velocity" : 120.12
+                }""";
+
+        Assertions.assertThrows(JsonMappingException.class,
+                                () -> mapper.readValue(carJson, Car.class));
+    }
+
     @Test
     void deserializeJsonFormFile() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         SpectrumChannelSetting model = objectMapper.readValue(getClass().getResourceAsStream("/anritsu-spectrum-setting.json"), SpectrumChannelSetting.class);
 
-        assertEquals("S-Band TX", model.getChannel1().getDescription());
-        assertEquals("S-Band RX RH", model.getChannel2().getDescription());
-        assertEquals("S-Band RX LH", model.getChannel3().getDescription());
-        assertEquals("X-Band RX RH", model.getChannel4().getDescription());
-        assertEquals("X-Band RX LH", model.getChannel5().getDescription());
+        Assertions.assertEquals("S-Band TX", model.getChannel1().getDescription());
+        Assertions.assertEquals("S-Band RX RH", model.getChannel2().getDescription());
+        Assertions.assertEquals("S-Band RX LH", model.getChannel3().getDescription());
+        Assertions.assertEquals("X-Band RX RH", model.getChannel4().getDescription());
+        Assertions.assertEquals("X-Band RX LH", model.getChannel5().getDescription());
 
         log.info(model.getChannel1().toString());
         log.info(model.getChannel2().toString());
@@ -32,6 +115,8 @@ public class JsonSerializeTest {
         log.info(model.getChannel4().toString());
         log.info(model.getChannel5().toString());
     }
+
+    @SuppressWarnings("DynamicRegexReplaceableByCompiledPattern")
     @Test
     void objectToJson() throws JsonProcessingException {
         Compose compose = new Compose(
@@ -42,7 +127,7 @@ public class JsonSerializeTest {
         ObjectMapper mapper = new ObjectMapper();
 
         String json = mapper.writeValueAsString(compose);
-        Assertions.assertThat(json).isEqualTo("""
+        Assertions.assertEquals("""
                 {
                   "unit1" : {
                     "name" : "A",
@@ -53,7 +138,7 @@ public class JsonSerializeTest {
                     "age" : 2
                   }
                 }
-                """.replaceAll("[\n ]", ""));
+                """.replaceAll("[\n ]", ""), json);
     }
 
     @RequiredArgsConstructor
