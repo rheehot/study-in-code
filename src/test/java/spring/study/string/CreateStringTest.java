770742a692485e512f527cce740107b33bd43cf3
Jsing <joosing711@gmail.com>
2023-01-25 13:11:04 +0900

    feat: String::valueOf 사용해 문자를 문자열로 만들기

---
+++ src/test/java/spring/study/string/CreateStringTest.java
@@ -0,0 +1,13 @@
+package spring.study.string;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CreateStringTest {
+    @Test
+    void createStringFromChar() {
+        char ch = 'a';
+        String result = String.valueOf(ch);
+        Assertions.assertThat(result).isEqualTo("a");
+    }
+}
