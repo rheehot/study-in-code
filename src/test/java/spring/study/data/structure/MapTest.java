787ebf83f501244ec12bea15f10a49cf57805665
Jsing <joosing711@gmail.com>
2023-02-02 08:50:32 +0900

    feat: Map::getOrDefault 학습 테스트

--- src/test/java/spring/study/data/structure/MapTest.java
+++ src/test/java/spring/study/data/structure/MapTest.java
@@ -1,13 +1,26 @@
 package spring.study.data.structure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MapTest {
+
+    @Test
+    void getOrDefault() {
+        // Given
+        Map<String, Integer> userAndAgeMap = new HashMap<>();
+        userAndAgeMap.put("Joo", 40);
+        userAndAgeMap.put("Yang", 39);
+
+        // Expected
+        Assertions.assertEquals(10, userAndAgeMap.getOrDefault("SSa", 10));
+        Assertions.assertEquals(40, userAndAgeMap.getOrDefault("Joo", 10));
+        Assertions.assertEquals(39, userAndAgeMap.getOrDefault("Yang", 10));
+    }
 
     @Test
     @DisplayName("Enum + 문자열 키 검증")
