f824605e2027dce90a84b1c9455fdc89895ad6ca
Jsing <joosing711@gmail.com>
2023-02-08 16:21:17 +0900

    feat: Slf4j 대신 System.out 패키지 활용하여 의존성 단순하게 표현하기

--- src/main/java/netty/log/ConsoleLog.java
+++ src/main/java/netty/log/ConsoleLog.java
@@ -3,21 +3,19 @@ package netty.log;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.extern.slf4j.Slf4j;
 import netty.handler.LogTarget;
 
-@Slf4j
 public class ConsoleLog implements LogTarget {
-    private final List<String> inputs = new ArrayList<>();
+    private final List<String> logged = new ArrayList<>();
 
     @Override
     public void log(String msg) {
-        log.info(msg);
-        inputs.add(msg);
+        System.out.println(msg);
+        logged.add(msg);
     }
 
     @Override
     public List<String> logged() {
-        return inputs;
+        return logged;
     }
 }
