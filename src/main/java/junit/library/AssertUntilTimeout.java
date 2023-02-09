bbd7b5ce993685d2e5c5303600c449dbeb096c81
Jsing <joosing711@gmail.com>
2023-02-09 20:45:16 +0900

    docs: 코파일럿이 만들어준 주석 추가

--- src/main/java/junit/library/AssertUntilTimeout.java
+++ src/main/java/junit/library/AssertUntilTimeout.java
@@ -15,6 +15,14 @@ public class AssertUntilTimeout {
         /* no-op */
     }
 
+    /**
+     * Asserts that the supplied {@code assertOnce} returns {@code true} within the
+     * given {@code timeout}.
+     *
+     * @param timeout the maximum time in milliseconds the code under test is allowed to run
+     * @param assertOnce the code under test
+     * @throws AssertionError if the code under test throws an exception or returns {@code false}
+     */
     public static void assertUntilTimeout(Duration timeout, ThrowingSupplier<Boolean> assertOnce, Supplier<String> messageSupplier) {
         long timeoutMillis = timeout.toMillis();
         long startMillis = System.currentTimeMillis();
