2966c43c3074b3b51c072be43cd6e8640c7050bd
Jsing <joosing711@gmail.com>
2023-02-10 14:12:18 +0900

    docs: (컴파일 되지 않은) 이슈 설명에 도움을 주기 위한 다양한 assertTimeout 사용 코드

---
+++ src/test/java/junit/library/SelfAssertUntilTimeoutTest.java
@@ -0,0 +1,56 @@
+package junit.library;
+
+public class SelfAssertUntilTimeoutTest {
+/*    Status lastStatus;
+    Channel channel;
+
+    @BeforeAll
+    void setUp() {
+        lastStatus = new Status();
+        channel = new Channel(lastStatus);
+        channel.connect().sync();
+    }
+
+    @Test
+    void changeStatusReflectsLastStatus() {
+        // When : send a command
+        channel.send(new ChangeStatus(100));
+
+        // Then : assert until it receives the expected result within timeout
+        long startMillis = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startMillis < 2000) {
+            if (lastStatus.getStatus() == 100) {
+                // success
+                return;
+            }
+        }
+
+        // fail
+        Assertions.fail();
+    }
+
+    @Test
+    void changeStatusReflectsLastStatus_ourLibraryVersion() {
+        // When : send a command
+        channel.send(new ChangeStatus(100));
+
+        // Then : assert until it receives the expected result within timeout
+        AssertUntilTimeout.assertUntilTimeout(ofMillis(2000),
+                                              () -> lastStatus.getStatus() == 100,
+                                              () -> "status : " + lastStatus.getStatus());
+    }
+
+    @Test
+    void changeStatusReflectsLastStatus_assertTimeoutVersion() {
+        // When : send a command
+        channel.send(new ChangeStatus(100));
+
+        // Then : assert until it receives the expected result within timeout
+        Assertions.assertTimeout(ofMillis(2000),
+                                 () -> {
+                                     while (lastStatus.getStatus() != 100) {
+                                         // interval if needed
+                                     }
+                                 }, () -> "status : " + lastStatus.getStatus());
+    }*/
+}
