a8e1b2bc8f979caa0ceaf59e3eddcede9b416b52
Jsing <joosing711@gmail.com>
2023-02-11 22:00:58 +0900

    Understand Awaitility conditionEvaluationListener feature

--- src/test/java/junit/writing/timeout/AssertUntilTimeoutTest.java
+++ src/test/java/junit/writing/timeout/AssertUntilTimeoutTest.java
@@ -1,16 +1,25 @@
 package junit.writing.timeout;
 
 import static java.time.Duration.ofMillis;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.fieldIn;
+import static org.awaitility.Awaitility.with;
+import static org.hamcrest.Matchers.equalTo;
 
+import org.awaitility.core.ConditionEvaluationListener;
+import org.awaitility.core.EvaluatedCondition;
+import org.awaitility.core.TimeoutEvent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import junit.library.AssertUntilTimeout;
 import junit.library.TryUntilTimeout;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AssertUntilTimeoutTest {
     FakeController fakeController;
 
@@ -78,13 +87,77 @@ public class AssertUntilTimeoutTest {
         );
     }
 
-    @Test
-    void statusReflectsCommandedValue_awaitility() {
-        // When : send a command to change target value
-        fakeController.sendCommand(100);
+    @Nested
+    class Awaitility {
 
-        // Then : The status reflects the commanded value within timeout
-        await().atMost(2, SECONDS).until(() -> fakeController.readStatus() == 100);
-        Assertions.assertEquals(100, fakeController.readStatus());
+        @Test
+        void statusReflectsCommandedValue_awaitility1() {
+            // When : send a command to change target value
+            fakeController.sendCommand(100, ofMillis(600));
+
+            // Then : The status reflects the commanded value within timeout
+            await().atMost(500, MILLISECONDS).until(() -> fakeController.readStatus() == 100);
+        }
+
+        @Test
+        void evaluationListener() {
+            // When : send a command to change target value
+            fakeController.sendCommand(100, ofMillis(300));
+
+            // Then : The status reflects the commanded value within timeout
+            with().conditionEvaluationListener(condition -> log.info("status on failure : {}", fakeController.readStatus()))
+                  .await()
+                  .atMost(500, MILLISECONDS)
+                  .until(() -> fakeController.readStatus() == 100);
+        }
+
+        @Test
+        void evaluationListener_onTimeout() {
+            // When : send a command to change target value
+            fakeController.sendCommand(100, ofMillis(600));
+
+            // Then : The status reflects the commanded value within timeout
+            with().conditionEvaluationListener(new ConditionEvaluationListener<>() {
+                @Override
+                public void onTimeout(TimeoutEvent timeoutEvent) {
+                    log.info("status of failure : {}", fakeController.readStatus());
+                }
+
+                @Override
+                public void conditionEvaluated(EvaluatedCondition condition) {}
+
+            }).await().atMost(500, MILLISECONDS).until(() -> fakeController.readStatus() == 100);
+        }
+
+        @Test
+        void statusReflectsCommandedValue_expected() {
+            // When : send a command to change target value
+            fakeController.sendCommand(100);
+
+            // Then : The status reflects the commanded value within timeout
+            await().atMost(100, MILLISECONDS)
+                   .until(() -> fakeController.readStatus() == 100);
+//                   .messageOnFailure(() -> "status : " + fakeController.readStatus());
+        }
+
+        @Test
+        void statusReflectsCommandedValue_int() {
+            // When : send a command to change target value
+            fakeController.sendCommand(100, ofMillis(600));
+
+            // Then : The status reflects the commanded value within timeout
+            await().atMost(500, MILLISECONDS).until(fieldIn(fakeController).ofType(int.class).andWithName("value"),
+                                                    equalTo(100));
+        }
+
+        @Test
+        void statusReflectsCommandedValue_awaitility2() {
+            // When : send a command to change target value
+            Message message = new Message("Joo", 100);
+            fakeController.sendCommand(message, ofMillis(600));
+
+            // Then : The status reflects the commanded value within timeout
+            await().atMost(500, MILLISECONDS).until(fieldIn(fakeController).ofType(Message.class).andWithName("message"), equalTo(message));
+        }
     }
 }
--- src/test/java/junit/writing/timeout/FakeController.java
+++ src/test/java/junit/writing/timeout/FakeController.java
@@ -1,20 +1,38 @@
 package junit.writing.timeout;
 
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
 /**
  * JUnit 오픈소스 프로젝트에 assertUntilTimeout 기능을 제한하기 위한 샘플 코드를 작성에 필요한 더미 의존 클래스를 정의합니다.
  */
-class FakeController {
+public class FakeController {
     // volatile 키워드를 선언하지 않는 경우 sendCommand() 메서드에서 value 필드를 변경하더라도
     // 다른 쓰레드에서 readStatus() 메서드를 호출하면 변경된 값을 읽지 못할 수 있습니다.
-    volatile int value;
+    private volatile int value;
+    private volatile Message message;
 
-    void connect() {
+    public void connect() {
         value = 0;
+        message = new Message("", 0);
     }
 
-    void sendCommand(int value) {
+    public void send(double j) {
+
+    }
+
+    public void sendCommand(int value, Duration delayMillis) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(delayMillis.toMillis());
+                this.value = value;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void sendCommand(int value) {
         CompletableFuture.runAsync(() -> {
             try {
                 Thread.sleep(1);
@@ -25,7 +43,22 @@ class FakeController {
         });
     }
 
-    int readStatus() {
+    public void sendCommand(Message message, Duration delayMillis) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(delayMillis.toMillis());
+                this.message = message;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public int readStatus() {
         return value;
+    }
+
+    public Message readMessage() {
+        return message;
     }
 }
