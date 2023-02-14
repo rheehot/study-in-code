5e1b3ea68b321a91309963f046cbf17c194dbbe9
Jsing <joosing711@gmail.com>
2023-02-14 14:42:11 +0900

    Describes how to calculate the sum in parallel using the Stream::parallel method

---
+++ src/test/java/javalang/stream/ParallelTest.java
@@ -0,0 +1,18 @@
+package javalang.stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.LongStream;
+
+import org.junit.jupiter.api.Test;
+
+public class ParallelTest {
+    @Test
+    void parallelSum() {
+        int n = 10_000_000;
+        Long result = LongStream.rangeClosed(1, n)
+                                .parallel()
+                                .reduce(0L, Long::sum);
+        assertEquals(50_000_005_000_000L, result);
+    }
+}
