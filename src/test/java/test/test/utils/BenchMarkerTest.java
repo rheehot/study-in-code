190a916ef8ae6458d3dbf0abbb9d69df98060ea5
Jsing <joosing711@gmail.com>
2023-02-15 13:24:28 +0900

    Create a performance measurement utility and test code

---
+++ src/test/java/test/test/utils/BenchMarkerTest.java
@@ -0,0 +1,33 @@
+package test.test.utils;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import test.utils.BenchMarker;
+
+public class BenchMarkerTest {
+
+    @Test
+    void benchMarker() throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(BenchMarker.class.getSimpleName())
+                .mode(Mode.AverageTime)
+                .timeUnit(TimeUnit.MILLISECONDS)
+                .jvmArgs("-Xms4G", "-Xmx4G")
+                .warmupIterations(1)
+                .warmupTime(TimeValue.seconds(2))
+                .measurementIterations(1)
+                .measurementTime(TimeValue.seconds(2))
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
