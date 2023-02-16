e298e2761a026aa22d3f7321412aae95a0639c3e
Jsing <joosing711@gmail.com>
2023-02-16 11:52:41 +0900

    Describes long type min and max values

---
+++ src/test/java/javalang/type/LongTypeTest.java
@@ -0,0 +1,19 @@
+package javalang.type;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LongTypeTest {
+
+    @Test
+    void maxValue () {
+        long max = Long.MAX_VALUE;
+        Assertions.assertEquals(9_223_372_036_854_775_807L, max);
+    }
+
+    @Test
+    void minValue () {
+        long min = Long.MIN_VALUE;
+        Assertions.assertEquals(-9_223_372_036_854_775_808L, min);
+    }
+}
