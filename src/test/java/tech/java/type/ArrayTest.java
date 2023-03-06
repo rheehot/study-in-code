78470a2c298fa9592f34aafbc0d2fd3fe07a293f
Jsing <joosing711@gmail.com>
2023-03-06 12:42:56 +0900

    Describe how to access two-dimensional array's columns and rows

---
+++ src/test/java/tech/java/type/ArrayTest.java
@@ -0,0 +1,22 @@
+package tech.java.type;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArrayTest {
+
+    @Test
+    void twoDimensionalArray() {
+        int[][] array = {  // = int [2][3]
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        Assertions.assertEquals(1, array[0][0]);
+        Assertions.assertEquals(2, array[0][1]);
+        Assertions.assertEquals(3, array[0][2]);
+        Assertions.assertEquals(4, array[1][0]);
+        Assertions.assertEquals(5, array[1][1]);
+        Assertions.assertEquals(6, array[1][2]);
+    }
+}
