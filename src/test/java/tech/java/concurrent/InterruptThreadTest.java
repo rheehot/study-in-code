43f49d10d421cfcddd1c937a4d09466bc2046f97
Jsing <joosing711@gmail.com>
2023-03-10 10:45:26 +0900

    Add descriptive code for the thread interrupt feature and comment out the description as well

--- src/test/java/tech/java/concurrent/InterruptThreadTest.java
+++ src/test/java/tech/java/concurrent/InterruptThreadTest.java
@@ -9,16 +9,20 @@ public class InterruptThreadTest {
     void interrupt() throws InterruptedException {
         Thread thread = new Thread(() -> {
             while (true) {
+                // isInterrupted 메서드는 인터럽트 플래그의 상태를 확인하기만 합니다.
+                // 반면에 interrupted 메서드는 인터럽트 플래그의 상태를 확인한 후, 인터럽트 플래그를 리셋합니다.
                 if (Thread.currentThread().isInterrupted()) {
-                    System.out.println("Interrupted!");
+                    System.out.println("Interrupted and current interrupt status is " + Thread.interrupted());
+                    System.out.println("But Thread.interrupted() call makes interrupt status to " + Thread.currentThread().isInterrupted());
                     break;
                 }
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    System.out.println("Interrupted while sleeping");
-                    // set the interrupted status
+                    System.out.println("Interrupted while sleeping by main thread and now interrupt status is reset to " + Thread.currentThread().isInterrupted());
+                    // interrupt 메서드는 기본적으로 쓰레드의 인터럽트 플래그를 설정합니다.
                     Thread.currentThread().interrupt();
+                    System.out.println("When handling to InterruptedException, re-interrupt the thread and now interrupt status is " + Thread.currentThread().isInterrupted());
                 }
                 Thread.yield();
             }
