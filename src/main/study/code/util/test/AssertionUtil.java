15f201418fcd57d547427f10c2ba3235fafe63ec
Jsing <joosing711@gmail.com>
2023-03-15 11:17:09 +0900

    Add a logging switch feature in AssertionsUtil

--- src/main/study/code/util/test/AssertionUtil.java
+++ src/main/study/code/util/test/AssertionUtil.java
@@ -1,36 +1,45 @@
 package code.util.test;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.Duration;
 import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 public final class AssertionUtil {
+    private static boolean logAvailable;
 
-    public static void assertIntervalWithRange(LocalTime startInclusive, LocalTime endExclusive, long targetMin, long targetMax) {
-        long interval = Duration.between(startInclusive, endExclusive).toMillis();
+    public static void setLogAvailable(boolean available) {
+        logAvailable = available;
+    }
+
+    public static void assertDurationWithRange(LocalTime startInclusive, LocalTime endExclusive, long targetMin, long targetMax) {
+        long duration = Duration.between(startInclusive, endExclusive).toMillis();
         try {
-            assertTrue(interval >= targetMin && interval <= targetMax);
+            assertTrue(duration >= targetMin && duration <= targetMax);
+            if (logAvailable) {
+                log.info("duration: {}, targetMin: {}, targetMax: {}", duration, targetMin, targetMax);
+            }
         } catch (AssertionError e) {
-            System.out.println("interval: " + interval);
-            System.out.println("targetMin: " + targetMin);
-            System.out.println("targetMax: " + targetMax);
+            log.info("AssertionError - duration: {}, targetMin: {}, targetMax: {}", duration, targetMin, targetMax);
             throw e;
         }
     }
 
-    public static void assertIntervalWithMargin(LocalTime startInclusive, LocalTime endExclusive, long target, long margin) {
-        long interval = Duration.between(startInclusive, endExclusive).toMillis();
+    public static void assertDurationWithMargin(LocalTime startInclusive, LocalTime endExclusive, long target, long margin) {
+        long duration = Duration.between(startInclusive, endExclusive).toMillis();
         try {
-            assertTrue(interval >= (target - margin) && interval <= (target + margin));
+            assertTrue(duration >= (target - margin) && duration <= (target + margin));
+            if (logAvailable) {
+                log.info("duration: {}, target: {}, margin: {}", duration, target, margin);
+            }
         } catch (AssertionError e) {
-            System.out.println("interval: " + interval);
-            System.out.println("target: " + target);
-            System.out.println("margin: " + margin);
+            log.info("AssertionError - duration: {}, target: {}, margin: {}", duration, target, margin);
             throw e;
         }
     }
-
 
     private AssertionUtil() {}
 }
