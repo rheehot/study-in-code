1d80d9cceb25e17aa5d3d3bbc73706df0e4ca6ad
Jsing <joosing711@gmail.com>
2023-03-27 15:39:34 +0900

    Describe how to handle the completion of multiple asynchronous tasks

--- src/test/study/tech/java/concurrent/CompletableFutureTest.java
+++ src/test/study/tech/java/concurrent/CompletableFutureTest.java
@@ -1,14 +1,54 @@
 package tech.java.concurrent;
 
+import code.util.SleepUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CompletableFutureTest {
+
+    @Nested
+    class AllOf {
+        List<CompletableFuture<Void>> futures;
+
+        @BeforeEach
+        void setUp() {
+            futures = List.of(
+                    CompletableFuture.runAsync(() -> {
+                        SleepUtil.sleep(500);
+                    }),
+                    CompletableFuture.runAsync(() -> {
+                        SleepUtil.sleep(1000);
+                    })
+            );
+        }
+
+        /**
+         * CompletableFuture allOf()를 사용해 여러 개의 비동기 작업 완료를 기다립니다.
+         */
+        @Test
+        void combineFutureByCompletable() {
+            await().atLeast(900, TimeUnit.MILLISECONDS)
+                    .until(() -> CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).isDone());
+        }
+
+        /**
+         * Stream allMatch()를 사용해 여러 개의 비동기 작업 완료를 기다립니다.
+         */
+        @Test
+        void combineFutureByStream() {
+            await().atLeast(900,TimeUnit.MILLISECONDS)
+                    .until(() -> futures.stream().allMatch(CompletableFuture::isDone));
+        }
+    }
 
     @Nested
     class ExceptionHandle {
